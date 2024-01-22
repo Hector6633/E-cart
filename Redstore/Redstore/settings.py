310631dfe6fd0f295301b04f6e68bddb3731f746
Rajeev <rajeevlakshmanan003@gmail.com>
2024-01-22 13:07:07 +0530

    Product-module completed.

--- Redstore/Redstore/settings.py
+++ Redstore/Redstore/settings.py
@@ -68,6 +68,10 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            #      'libraries':{
+            # 'spliter': 'app.templatetags.spliter',
+            
+            # }
         },
     },
 ]
